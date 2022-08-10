5b9fcecfafceb37ccc5ade76ac8288f1bee49bce
kisnj <knandor407@gmail.com>
2022-08-10 16:10:24 +0200

    length fix

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose= require("mongoose")
 const Schema=mongoose.Schema
 
 const User=new Schema({
-    username:{type:String,required:true,trim:true,maxLenght:20},
+    username:{type:String,required:true,trim:true,maxLength:20},
     password:{type:String,required:true},
     status:{type:String,required:true,enum:["normal","member","admin"]},
     profile_picture:{type:Schema.Types.ObjectId,required:true,ref:"Profile_Picture"}
